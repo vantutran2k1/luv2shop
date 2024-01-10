5a83bf87d31d8a94fea6e3e051c9803dfc62348a
vantutran2k1 <vantu.tran2k1@gmail.com>
2024-01-10 12:34:56 +0700

    Intergrate template for the UI

--- backend/src/main/java/com/tutran/backend/api/repository/ProductCategoryRepository.java
+++ backend/src/main/java/com/tutran/backend/api/repository/ProductCategoryRepository.java
@@ -3,7 +3,9 @@ package com.tutran.backend.api.repository;
 import com.tutran.backend.api.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(path = "product-categories")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
