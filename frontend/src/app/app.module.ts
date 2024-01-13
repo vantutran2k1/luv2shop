f876dfa012c67bc9219ae228939d13429c6727f1
vantutran2k1 <vantu.tran2k1@gmail.com>
2024-01-13 23:49:29 +0700

    Add Okta service for authenticating user

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -15,8 +15,18 @@ import {CartStatusComponent} from './components/cart-status/cart-status.componen
 import {CartDetailsComponent} from './components/cart-details/cart-details.component';
 import {CheckoutComponent} from './components/checkout/checkout.component';
 import {ReactiveFormsModule} from "@angular/forms";
+import {LoginComponent} from './components/login/login.component';
+import {LoginStatusComponent} from './components/login-status/login-status.component';
+import AppConfig from "./config/app-config";
+import {OktaAuth} from "@okta/okta-auth-js";
+import {OKTA_CONFIG, OktaAuthModule, OktaCallbackComponent} from "@okta/okta-angular";
+
+const oktaConfig = AppConfig.oidc;
+const oktaAuth = new OktaAuth(oktaConfig);
 
 const routes: Routes = [
+  {path: 'login/callback', component: OktaCallbackComponent},
+  {path: 'login', component: LoginComponent},
   {path: 'checkout', component: CheckoutComponent},
   {path: 'cart-details', component: CartDetailsComponent},
   {path: 'products/:id', component: ProductDetailsComponent},
@@ -36,7 +46,9 @@ const routes: Routes = [
     ProductDetailsComponent,
     CartStatusComponent,
     CartDetailsComponent,
-    CheckoutComponent
+    CheckoutComponent,
+    LoginComponent,
+    LoginStatusComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
@@ -44,9 +56,10 @@ const routes: Routes = [
     HttpClientModule,
     NgOptimizedImage,
     NgbModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    OktaAuthModule
   ],
-  providers: [ProductService],
+  providers: [ProductService, {provide: OKTA_CONFIG, useValue: {oktaAuth}}],
   bootstrap: [AppComponent]
 })
 export class AppModule {
---
+++ frontend/src/app/config/app-config.ts
@@ -0,0 +1,8 @@
+export default {
+  oidc: {
+    clientId: '0oaec9viqk34YPnLo5d7',
+    issuer: 'https://dev-18938895.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:4200/login/callback',
+    scopes: ['openid', 'profile', 'email']
+  }
+}
