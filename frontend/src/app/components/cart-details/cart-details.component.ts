7ccd9cda3161dbaf923ba4671e9049d51bd3d823
vantutran2k1 <vantu.tran2k1@gmail.com>
2024-01-13 09:55:57 +0700

    Display cart details of products and total price, quantity

---
+++ frontend/src/app/components/cart-details/cart-details.component.ts
@@ -0,0 +1,30 @@
+import {Component, OnInit} from '@angular/core';
+import {CartItem} from "../../common/cart-item";
+import {CartService} from "../../services/cart.service";
+
+@Component({
+  selector: 'app-cart-details',
+  templateUrl: './cart-details.component.html',
+  styleUrls: ['./cart-details.component.css']
+})
+export class CartDetailsComponent implements OnInit {
+  cartItems: CartItem[] = [];
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
+
+  constructor(private cartService: CartService) {
+  }
+
+  ngOnInit(): void {
+    this.listCartDetails();
+  }
+
+  listCartDetails() {
+    this.cartItems = this.cartService.cartItems;
+
+    this.cartService.totalPrice.subscribe(totalPrice => this.totalPrice = totalPrice);
+    this.cartService.totalQuantity.subscribe(totalQuantity => this.totalQuantity = totalQuantity);
+
+    this.cartService.computeCartTotals();
+  }
+}
