c38f4b903c44aac9bbb2ca7d686118d69433a927
vantutran2k1 <vantu.tran2k1@gmail.com>
2024-01-14 15:01:21 +0700

    Secure communication with HTTPS from frontend

--- frontend/src/app/config/app-config.ts
+++ frontend/src/app/config/app-config.ts
@@ -2,7 +2,7 @@ export default {
   oidc: {
     clientId: '0oaec9viqk34YPnLo5d7',
     issuer: 'https://dev-18938895.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:4200/login/callback',
+    redirectUri: 'https://localhost:4200/login/callback',
     scopes: ['openid', 'profile', 'email']
   }
 }
